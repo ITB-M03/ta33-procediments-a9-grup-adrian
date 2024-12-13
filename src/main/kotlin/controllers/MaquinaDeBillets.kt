a75b24f19f99e9a48f36489593457372a46eaaf4
Gvlinsogv <adrian.galinsoga.7e6@itb.cat>
2024-12-13 10:06:04 +0100

    FirstCommit

---
+++ src/main/kotlin/controllers/MaquinaDeBillets.kt
@@ -0,0 +1,21 @@
+package org.example.controllers
+
+
+/**
+ * @author Adrián Galinsoga
+ * @date 10/12/2024
+ *
+ * Màquina de venda de bitllets de transport dels FGC i TMB
+ *
+ * Objectius:
+ * Els objectius d’aquest exercici són:
+ * • Treballar estructures condicionals
+ * • Treballar estructures iteratives
+ * • Implementar estructures de control d’input de dades
+ * • La programació modular
+ *
+ *
+ */
+fun main() {
+}
+
